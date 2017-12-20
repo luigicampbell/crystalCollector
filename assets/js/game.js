bc9b6e20b9ac728e5362873a3d953c32911b9e2a
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2017-12-20 09:49:38 -0800

    added formatting and css to where crystals, scores to match and instructions will go

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,37 @@
-
-var target = function(){
+// Logic for Crystals
+var crystal1 = function(){
   var a = Math.floor((Math.random() * 120) + 19);
   return a;
   console.log(a);
 }
 
-target();
+var crystal2 = function(){
+  var a = Math.floor((Math.random() * 120) + 19);
+  return a;
+  console.log(a);
+}
 
-$('#randomNumber').text(target());
+var crystal3 = function(){
+  var a = Math.floor((Math.random() * 120) + 19);
+  return a;
+  console.log(a);
+}
+
+var crystal4 = function(){
+  var a = Math.floor((Math.random() * 120) + 19);
+  return a;
+  console.log(a);
+}
+
+//Calling the functions for the crystals
+crystal1();
+crystal2();
+crystal3();
+crystal4();
+
+//printing them in the div
+$('#crystal1').text(crystal1());
+$('#crystal2').text(crystal2());
+$('#crystal3').text(crystal3());
+$('#crystal4').text(crystal4());
 console.log();
