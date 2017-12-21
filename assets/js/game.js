36beddbab81b8220f0b0c3a64e4e04cac47d96c4
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2017-12-21 11:49:10 -0800

    working on functions layout and how to get them to behave sequentially

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,34 +3,43 @@ var total = 0;
 var win = 0;
 var lose = 0;
 var addedCrystalvalue = 0;
-
-
-// Logic for Crystals use array to store crystals
-//for loop that iterates through each index of the crystal array
-//
-//access each index [i], then perform Math.random function for each crystal
-//
 var crystalArr =[];
-while(crystalArr.length < 4){
-  var randomCrystalValue = Math.floor(Math.random()*12) + 1;
-  if (crystalArr.indexOf(randomCrystalValue) > -1) continue;
+var randomNumber;
 
-  //randomizes value of crystal
-  crystalArr[crystalArr.length] = randomCrystalValue;
-}
+//Shuffles the Array after Win/Lose
+function shuffle() {
 
+  while(crystalArr.length < 4){
+    var randomCrystalValue = Math.floor(Math.random()*12) + 1;
+    if (crystalArr.indexOf(randomCrystalValue) > -1) continue;
+
+    //randomizes value of crystal
+    crystalArr[crystalArr.length] = randomCrystalValue;
+  }
 console.log(crystalArr);
 
-var randomNumber = Math.floor(Math.random()*101+19);
+  //Score to Match
+  randomNumber = Math.floor(Math.random()*101+19);
+}
 
-//printing them in the div
+//Calling My Functions
+shuffle();
+// $("#wins").text("wins: " + win);
+// $("#losses").text(" losses: " + lose);
+
+
+//write some animations for instructions
+
+//Printing Score to Match
 $("#randomNumber").text(randomNumber);
+
 $("#crystal1img").click(function clickCrystal(){
   addedCrystalvalue = crystalArr[0];
   total = total + addedCrystalvalue;
   $("#userTotal").text(total);
 });
-//testing output for score matcher
+
+//Testing Output for Score Matcher
 console.log(total);
 
 $("#crystal2img").click(function clickCrystal(){
@@ -50,3 +59,20 @@ $("#crystal4img").click(function clickCrystal(){
   total = total + addedCrystalvalue;
   $("#userTotal").text(total);
 });
+
+//Win and Lose Conditions
+if (total > randomNumber) {
+  lose ++;
+  shuffle();
+  total = 0;
+  $("#losses").text("losses: " + lose);
+}
+console.log("losses: " + lose);
+
+if (total =  randomNumber) {
+  win ++;
+  shuffle();
+  total = 0;
+  $("#wins").text("wins: " + win);
+}
+console.log("wins: " + win);
