748400983c844728c77757093642394dee9ee31a
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2017-12-23 11:28:13 -0800

    diagnosing where function calls belong

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,76 +1,76 @@
-//Variables
-var total = 0;
-var win = 0;
-var lose = 0;
-var addedCrystalvalue = 0;
-var crystalArr =[];
-
-//Shuffles the Array after Win/Lose
-
-function shuffle() {
-  //Stores Random Number to Match
-  randomNumber = Math.floor(Math.random()*101+19);
-  $("#matchMe").text(" "+ randomNumber);
-
-  while(crystalArr.length < 4){
-    var randomCrystalValue = Math.floor(Math.random()*12) + 1;
-    if (crystalArr.indexOf(randomCrystalValue) > -1) continue;
-
-    //Randomizes Value of Crystal
-    crystalArr[crystalArr.length] = randomCrystalValue;
-  }
-  console.log(crystalArr);
-}
-//array is empty here
-console.log(crystalArr);
-//Function For Buttons
-//Adds Crystals to User Total
-function crystalButtons(){
-  $("#crystal1img").hover(function clickCrystal(){
-    addedCrystalvalue = crystalArr[0];
-    total = total + addedCrystalvalue;
-    $("#userTotal").text(total);
-  });
-
-  $("#crystal2img").hover(function clickCrystal(){
-    addedCrystalvalue = crystalArr[1];
-    total = total + addedCrystalvalue;
-    $("#userTotal").text(total);
-  });
-
-  $("#crystal3img").hover(function clickCrystal(){
-    addedCrystalvalue = crystalArr[2];
-    total = total + addedCrystalvalue;
-    $("#userTotal").text(total);
-  });
-
-  $("#crystal4img").hover(function clickCrystal(){
-    addedCrystalvalue = crystalArr[3];
-    total = total + addedCrystalvalue;
-    $("#userTotal").text(total);
-  });
-}
-//My array is empty here
-console.log(crystalArr);
-
-//Calling My Functions
-shuffle();
-crystalButtons();
-//write some animations for instructions
-function checkUserTotal(){
-  if (total > randomNumber) {
-    lose ++;
-    shuffle();
-    total = 0;
-    $("#losses").text("losses: " + lose);
-  } else if (total =  randomNumber) {
-    win ++;
-    shuffle();
-    total = 0;
-    $("#wins").text("wins: " + win);
-  }
-}
-checkUserTotal();
-console.log("wins: " + win);
-
-console.log("losses: " + lose);
+// //Variables
+// var total = 0;
+// var win = 0;
+// var lose = 0;
+// var addedCrystalvalue = 0;
+// var crystalArr =[];
+//
+// //Shuffles the Array after Win/Lose
+//
+// function shuffle() {
+//   //Stores Random Number to Match
+//   randomNumber = Math.floor(Math.random()*101+19);
+//   $("#matchMe").text(" "+ randomNumber);
+// 
+//   while(crystalArr.length < 4){
+//     var randomCrystalValue = Math.floor(Math.random()*12) + 1;
+//     if (crystalArr.indexOf(randomCrystalValue) > -1) continue;
+//
+//     //Randomizes Value of Crystal
+//     crystalArr[crystalArr.length] = randomCrystalValue;
+//   }
+//   console.log(crystalArr);
+// }
+// //array is empty here
+// console.log(crystalArr);
+// //Function For Buttons
+// //Adds Crystals to User Total
+// function crystalButtons(){
+//   $("#crystal1img").hover(function clickCrystal(){
+//     addedCrystalvalue = crystalArr[0];
+//     total = total + addedCrystalvalue;
+//     $("#userTotal").text(total);
+//   });
+//
+//   $("#crystal2img").hover(function clickCrystal(){
+//     addedCrystalvalue = crystalArr[1];
+//     total = total + addedCrystalvalue;
+//     $("#userTotal").text(total);
+//   });
+//
+//   $("#crystal3img").hover(function clickCrystal(){
+//     addedCrystalvalue = crystalArr[2];
+//     total = total + addedCrystalvalue;
+//     $("#userTotal").text(total);
+//   });
+//
+//   $("#crystal4img").hover(function clickCrystal(){
+//     addedCrystalvalue = crystalArr[3];
+//     total = total + addedCrystalvalue;
+//     $("#userTotal").text(total);
+//   });
+// }
+// //My array is empty here
+// console.log(crystalArr);
+//
+// //Calling My Functions
+// shuffle();
+// crystalButtons();
+// //write some animations for instructions
+// function checkUserTotal(){
+//   if (total > randomNumber) {
+//     lose ++;
+//     shuffle();
+//     total = 0;
+//     $("#losses").text("losses: " + lose);
+//   } else if (total =  randomNumber) {
+//     win ++;
+//     shuffle();
+//     total = 0;
+//     $("#wins").text("wins: " + win);
+//   }
+// }
+// checkUserTotal();
+// console.log("wins: " + win);
+//
+// console.log("losses: " + lose);
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,8 @@
+//Variables
+var total = 0;
+var win = 0;
+var lose = 0;
+//Crystal Object
 var crystal = {
   diamond:
   {
@@ -20,21 +25,62 @@ var crystal = {
     value: 0
   }
 };
-
+//Shuffles Values for Crystals
+function shuffle (){
 crystal.diamond.value = Math.floor(Math.random()*12)+1;
 crystal.sapphire.value = Math.floor(Math.random()*12)+1;
 crystal.ruby.value = Math.floor(Math.random()*12)+1;
 crystal.emerald.value = Math.floor(Math.random()*12)+1;
-
-
 console.log(crystal.diamond.value);
 console.log(crystal.sapphire.value);
 console.log(crystal.ruby.value);
 console.log(crystal.emerald.value);
-// var shuffleValues = {
-//   a:
-//   b:
-//   c:
-//   d:
-//   }
-// };
+};
+//Random Number for User to Match
+randomNumber = Math.floor(Math.random()*101+19);
+$("#matchMe").text(" "+ randomNumber);
+
+//Crystal Buttons attaching Value to Crystal Click
+function crystalButtons(){
+  $("#crystal1img").click(function clickCrystal(){
+    addedCrystalvalue = crystal.diamond.value;
+    total = total + addedCrystalvalue;
+    $("#userTotal").text(total);
+  });
+
+  $("#crystal2img").click(function clickCrystal(){
+    addedCrystalvalue = crystalArr[1];
+    total = total + addedCrystalvalue;
+    $("#userTotal").text(total);
+  });
+
+  $("#crystal3img").click(function clickCrystal(){
+    addedCrystalvalue = crystalArr[2];
+    total = total + addedCrystalvalue;
+    $("#userTotal").text(total);
+  });
+
+  $("#crystal4img").click(function clickCrystal(){
+    addedCrystalvalue = crystalArr[3];
+    total = total + addedCrystalvalue;
+    $("#userTotal").text(total);
+  });
+  checkUserTotal();
+
+};
+//Checks if User Total Matches Random Number for Win/Lose Condition
+function checkUserTotal(){
+  if (total > randomNumber) {
+    lose ++;
+    shuffle();
+    total = 0;
+    $("#losses").text("losses: " + lose);
+  } else if (total =  randomNumber) {
+    win ++;
+    shuffle();
+    total = 0;
+    $("#wins").text("wins: " + win);
+  }
+};
+//Functions
+crystalButtons();
