51900e98f78107597156e1f5f9b792c7429295be
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2017-12-23 11:05:19 -0800

    working on crystal bject and renaming values

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,40 @@
-vary crystal = {
-  crystal1{
-  value: 0;
+var crystal = {
+  diamond:
+  {
+    name: "Diamond",
+    value: 0
   },
-  crystal2{
-  value: 0
+  sapphire:
+  {
+    name: "Sapphire",
+    value: 0
   },
-  crystal3{
-  value: 0
+  ruby:
+  {
+    name: "Ruby",
+    value: 0
   },
-  crystal4{
+  emerald:
+  {
+    name: "Emerald",
     value: 0
   }
 };
+
+crystal.diamond.value = Math.floor(Math.random()*12)+1;
+crystal.sapphire.value = Math.floor(Math.random()*12)+1;
+crystal.ruby.value = Math.floor(Math.random()*12)+1;
+crystal.emerald.value = Math.floor(Math.random()*12)+1;
+
+
+console.log(crystal.diamond.value);
+console.log(crystal.sapphire.value);
+console.log(crystal.ruby.value);
+console.log(crystal.emerald.value);
+// var shuffleValues = {
+//   a:
+//   b:
+//   c:
+//   d:
+//   }
+// };
